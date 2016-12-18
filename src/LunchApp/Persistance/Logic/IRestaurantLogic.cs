e9ec7f4f7dd52ad81945e80e0d9cfd681018569f
Yexur <shimojax@gmail.com>
2016-12-18 05:11:13 -0600

    revert db connection changes
    revert db connection changes

---
+++ src/LunchApp/Persistance/Logic/IRestaurantLogic.cs
@@ -0,0 +1,14 @@
+﻿using LunchApp.Models.ApplicationModels;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LunchApp.Persistance.Logic
+{
+    public interface IRestaurantLogic
+    {
+        RestaurantModel FindById(int id);
+        Task<List<RestaurantModel>> GetList();
+        Task Save(RestaurantModel restaurant);
+        void Delete(int id);
+    }
+}
---
+++ src/LunchApp/Persistance/Logic/RestaurantLogic.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LunchApp.Models.ApplicationModels;
+using LunchApp.Persistance.Repository;
+
+namespace LunchApp.Persistance.Logic
+{
+    public class RestaurantLogic : IRestaurantLogic
+    {
+        private readonly IRestaurantRepository _restaurantRepository;
+
+        public RestaurantLogic(IRestaurantRepository restaurantRepository)
+        {
+            _restaurantRepository = restaurantRepository;
+        }
+
+        public void Delete(int id)
+        {
+            _restaurantRepository.Delete(id);
+        }
+
+        public RestaurantModel FindById(int id)
+        {
+           return _restaurantRepository.FindById(id);
+        }
+
+        public async Task<List<RestaurantModel>> GetList()
+        {
+            var restaurants = await _restaurantRepository.All();
+
+            if (restaurants == null || !restaurants.Any()) 
+            {
+                return Enumerable.Empty<RestaurantModel>().ToList();
+            }
+            return restaurants;
+        }
+
+        public async Task Save(RestaurantModel restaurant)
+        {
+            await _restaurantRepository.Insert(restaurant);
+        }
+    }
+}
